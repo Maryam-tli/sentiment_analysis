c33aa67bac8a52b0476fbe9cc157a3bb3715f1a0
Maryam-tli <m.tavakoolii1400@gmail.com>
2024-11-14 10:18:09 +0330

    Define Mood dataclass to store emoji and sentiment score

--- sentiment.py
+++ sentiment.py
@@ -1,3 +1,10 @@
 # Importing required libraries
 from textblob import TextBlob
 from dataclasses import dataclass
+
+
+# Define the Mood class
+@dataclass
+class Mood:
+    emoji: str
+    sentiment: float
